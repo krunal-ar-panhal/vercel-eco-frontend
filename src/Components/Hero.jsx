955b535b52eef8c9fab93819d47ce65947dea1cc
krunal-ar-panhal <krunalapnchalkp2123@gmail.com>
2024-11-11 12:32:28 +0530

    hero section create kiya

---
+++ src/Components/Hero.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Hero = () => {
+  return (
+    <div className='flex flex-col sm:flex-row border border-gray-400'>
+      {/* Hero Left Side */}
+      <div className='w-full sm:w-1/2 flex items-center justify-center py-10 sm:py-0'>
+        <div className='text-[#414141]'>
+          <div className='flex items-center gap-2'>
+            <p className='w-8 md:w-11 h-[2px] bg-[#414141]'></p>
+            <p className='font-medium text-sm md:text-base'>OUR BESTSELLERS</p>
+          </div>
+          <h1 className='prata-regular text-3xl sm:py-3 lg:text-5xl leading-relaxed'>
+            Discover Our Collection
+          </h1>
+          <div className='flex items-center gap-2 mt-4'>
+            <p className='font-semibold text-sm md:text-base'>SHOP NOW</p>
+            <p className='w-8 md:w-11 h-[1px] bg-[#414141]'></p>
+          </div>
+        </div>
+      </div>
+      {/* Hero Right Side */}
+      <img className='w-full sm:w-1/2' src='./hero_img.png' alt="Hero" />
+    </div>
+  );
+}
+
+export default Hero;
